30986aafdedd8d94449609419c4390a46062fb2e
Sir <grewalamu1@gmail.com>
2016-07-19 21:52:00 -0700

    Working To-Do List with add and remove.

--- To-DoList/FirstViewController.swift
+++ To-DoList/FirstViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    
+    @IBOutlet weak var table: UITableView!
+    
+    override func viewDidAppear(animated: Bool) {
+        table.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return toDoList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoList [indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
+            
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.Delete
+        {
+            toDoList.removeAtIndex(indexPath.row)
+            table.reloadData()
+        }
+        
+        
+    }
 
 }
 
--- To-DoList/SecondViewController.swift
+++ To-DoList/SecondViewController.swift
@@ -15,7 +15,22 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var task: UITextField!
+    @IBAction func addTask(sender: AnyObject) {
+        
+        toDoList.append(task.text!)
+        task.text = ""
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "arr")
+        
+    }
+    
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
+       
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
